7eb35e2fe92c14087e29af2c7d6d30c1b6ad3073
Artem Serga <artem@serga.name>
2017-07-08 14:41:29 +0300

    Use static graphql queries

--- typings.d.ts
+++ typings.d.ts
@@ -1,16 +1,16 @@
 declare module "react-masonry-infinite" {
-    var MasonryInfiniteScroller: any;
+  let MasonryInfiniteScroller: any;
 }
 
 declare module "react-ripples" {
-    var Ripples: any;
+  let Ripples: any;
 }
 
 declare module "react-lazy-load" {
-    var LazyLoad: {
-        offset?: number;
-        throttle?: number;
-        height?: string | number;
-        width?: string | number;
-    };
+  let LazyLoad: {
+    offset?: number;
+    throttle?: number;
+    height?: string | number;
+    width?: string | number;
+  };
 }
